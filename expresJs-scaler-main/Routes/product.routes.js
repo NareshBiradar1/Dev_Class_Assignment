d83c7cd6d55685cdfb157a4674fde30a5fb4a93f
NareshBiradar1 <nareshdb555@gmail.com>
2024-07-08 12:08:29 +0530

    8 july

---
+++ expresJs-scaler-main/Routes/product.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const productControllers = require('../Controllers/product.controller');
+
+// Create a product
+router.post('/', productControllers.createProduct);
+
+// Get all products
+router.get('/', productControllers.getAllProducts);
+
+// Get a product by product id
+router.get('/:id', productControllers.getProductById);
+
+// Update Product Details
+router.put('/:id', productControllers.updateProductById);
+
+// Delete a product by id
+router.delete('/:id', productControllers.deletedProductById);
+
+module.exports = router;
---
+++ expresJs-scaler-main/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./Routes/product.routes');
+const userRoutes = require('./Routes/user.routes');
+
+require('dotenv').config();
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then (() => {
+    console.log("DB Connected.");
+}).catch((err) => {
+    console.log("Failed", err);
+})
+
+// Middleware function (converts string to json format)
+app.use(express.json());
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+// app.use(middleware);
+// app.use(requestLogger);
+
+// let courses = [
+//     {id : 1, name : 'java'},
+//     {id : 2, name : 'javascript'},
+//     {id : 3, name : 'python'}
+// ];
+// app.get('/courses', (req, res) => {
+//     res.json(courses);
+// });
+
+// app.post('/courses', (req, res) => {
+//    const name = req.body.name;
+//    const newCourse = {id : courses.length+1, name : name};
+//    courses.push(newCourse);
+//    res.send(courses);
+// })
+
+// Update id 1, name : name --> spring
+// Delete id 2 course;
+// app.put('/courses', (req, res) => {
+//     try {
+//         const id = req.body.id;
+//         const name = req.body.name;
+//         for (let j=0; j<courses.length; j++){
+//             if (courses[j].id === id){
+//                 courses[j].name = name;
+//             }
+//         }
+//         res.send(courses);
+//     } catch (err) {
+//         res.status(505).send(err);
+//     }
+// })
+
+// app.delete('/courses', (req, res) => {
+//     const id = req.body.id;
+//     for (let j=0; j<courses.length; j++){
+//         if (courses[j].id === id){
+//             courses.splice(j, 1);
+//         }
+//     }
+//     res.send(courses);
+// })
+
+// Creating Custom Middlewear
+// function middleware(req, res, next){
+//     console.log("called");
+//     next();
+// }
+// logger middleware
+// method, ip, hostname, date
+// function requestLogger(req, res, next){
+//     console.log(req.ip);
+//     console.log(req.hostname);
+//     console.log(req.method);
+//     console.log(new Date().toISOString());
+//     next();
+// }
+const host = 'localhost';
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server has started.');
+
+})
+
+
+// MVC Architecture
+// Models View Controller
